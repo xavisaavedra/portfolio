4b81845a9a461cb3c6a6aa58826dc699968fa5b2
Xavi Saavedra Alois <33299408+xavisaavedra@users.noreply.github.com>
2023-11-16 15:01:04 +0000

    Refactor code for improved readability and performance.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Xavi Saavedra",
+  title: "Consultor SharePoint",
+  email: "xavi.saavedra.alois@gmail.com",
+  gitHub: "xavisaavedra",
+  linkedIn: "xavisaavedra",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Aprendiendo SPFx/REACT 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Mis primeros proyectos creados con SPFx/REACT para WebParts de SharePoint Online. En este repositorio encontrarás los proyectos que he ido creando para aprender a desarrollar con SPFx/REACT.",
+    url: "https://github.com/xavisaavedra/SPFx",
   },
 ];
 
